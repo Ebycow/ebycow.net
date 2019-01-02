506d314a77c7419869e5709ef1471cb5993645c7
Ebycow <18446038+Ebycow@users.noreply.github.com>
2019-01-02 13:47:32 +0900

    シーズンに対応

---
+++ src/bgchanger.ts
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+import moment from 'moment';
+
+//images
+const summer = require('./images/img2.jpg');
+const swimsuit = require('./images/img3.jpg');
+const subimg = require('./images/img4').img;
+
+
+let img: any | undefined = undefined;
+
+const month: number = parseInt(moment().format('MM'));
+if(month >= 6 && month <= 10){
+    img = summer;
+
+}
+
+const rnd: number = Math.random();
+if(rnd < 0.01){
+    img = swimsuit;
+
+}
+
+if(img !== undefined) {
+    $('.neko').css({
+        'background-image' : `url(${img})`
+    });
+
+}
+
+
+setTimeout(() => {
+    if(rnd < 0.001){
+        $('.neko').css({
+            'background-image' : `url(${subimg})`
+        });
+
+    }
+
+}, 60000);
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,15 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './styles/style.css';
 
 // fonts
-import './styles/googlefonts.css';
+
 
 // scripts
 import 'bootstrap';
-
 import $ from 'jquery';
+import './bgchanger';
 
+// tooltip
 $(() => {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
 
 });
