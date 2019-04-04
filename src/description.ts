5f1352e6ae7955850b38636306876f05894745c3
18446038+Ebycow@users.noreply.github.com <18446038+Ebycow@users.noreply.github.com>
2019-04-04 17:37:48 +0900

    delayは連続でホバーイベントが発行されたときに対応できないので妥協

--- src/description.ts
+++ src/description.ts
@@ -6,34 +6,23 @@ const defaultText = descElem.text();
 $('.link-item').hover(
     // on
     (ev) => {
-        //console.log(ev.currentTarget.getAttribute('data-original-title') + "");
         const linkTx = ev.currentTarget.getAttribute('title');
-
-
-        descElem.removeClass('description-default');
-        
-        descElem.addClass('description-link');
-
         if(linkTx !== null){
-            descElem.delay(250).queue(() => {
-                descElem.text(`#${linkTx}`).dequeue();
-
-            });
+            descElem.text(`#${linkTx}`).dequeue();
 
         }
-
+        
+        descElem.removeClass('description-default');
+        descElem.addClass('description-link');
 
     },
     
     // out
     (ev) => {
-
         descElem.text(defaultText);
+
         descElem.removeClass('description-link');
-
         descElem.addClass('description-default');
-
-
 
     }
 
