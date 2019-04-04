88cc43fb85c9f065a57ce05aad03e02c7d262506
18446038+Ebycow@users.noreply.github.com <18446038+Ebycow@users.noreply.github.com>
2019-04-04 20:06:22 +0900

    縦スクロールのアニメーションを改良

--- src/description.ts
+++ src/description.ts
@@ -3,26 +3,31 @@ import $ from 'jquery';
 const descElem = $('.description');
 const defaultText = descElem.text();
 
+let appendText: string = "";
+
+descElem.on('animationend', (ev) => {
+    const animationName: 'falldown' | 'scrolldown' | 'fallback' = ev.originalEvent.animationName;
+    if(animationName === 'falldown') {
+        descElem.text(`#${appendText}`);
+        
+    }
+
+});
+
 $('.link-item').hover(
     // on
     (ev) => {
-        const linkTx = ev.currentTarget.getAttribute('title');
-        if(linkTx !== null){
-            descElem.text(`#${linkTx}`).dequeue();
-
-        }
-        
-        descElem.removeClass('description-default');
-        descElem.addClass('description-link');
+        appendText = ev.currentTarget.getAttribute('data-title') + "";
+        descElem.toggleClass('link');
+        descElem.toggleClass('default');
 
     },
     
     // out
     (ev) => {
         descElem.text(defaultText);
-
-        descElem.removeClass('description-link');
-        descElem.addClass('description-default');
+        descElem.toggleClass('link');
+        descElem.toggleClass('default');
 
     }
 
