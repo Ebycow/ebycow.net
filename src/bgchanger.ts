9b13f19ffa9258a1a65eae8283cc44901f82ff06
Ebycow <18446038+Ebycow@users.noreply.github.com>
2022-11-01 22:51:16 +0900

    背景変更 アイコン変更 リンク変更 自己紹介追加

--- src/bgchanger.ts
+++ src/bgchanger.ts
@@ -3,35 +3,10 @@ import moment from 'moment';
 import CryptoJS from 'crypto-js';
 
 //images
-const normal = require('./images/img1.jpg');
-const summer = require('./images/img2.jpg');
-const swimsuit = require('./images/img3.jpg');
-const blazer = require('./images/img4.jpg');
-const sub = require('./images/img4').img;
+const normal = require('./images/1.png');
 
 let img: any | undefined = normal;
 
-const month: number = parseInt(moment().format('MM'));
-if(month >= 6 && month <= 10){
-    img = summer;
-
-}
-
-if(document.referrer === 'https://t.co/yzAoSQXM2s') {
-    img = blazer;
-    
-}
-
-const rnd: number = Math.random();
-if(rnd < 0.01){
-    img = swimsuit;
-
-}
-
-if(rnd < 0.001){
-    img = undefined;
-
-}
 
 if(img !== undefined) {
     $('.neko').css({
@@ -41,17 +16,3 @@ if(img !== undefined) {
 
 }
 
-
-setTimeout(() => {
-    if(rnd < 0.001){
-        const value = 110312180975097;
-        const _ = (`${value}`).split(/3|5|8/);
-
-        $('.neko').css({
-            'background-image' : `url(data:image/jpeg;base64,${CryptoJS.AES.decrypt(sub, String.fromCharCode(~~_[1*0],-(-_[2-1]),-_[~~2]*-1,+_[~~Math.PI])).toString(CryptoJS.enc.Utf8)})`
-
-        });
-
-    }
-
-}, 600000);
