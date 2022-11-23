f43216013765747fc05f7e1521245f41b26eb856
Ebycow <18446038+Ebycow@users.noreply.github.com>
2022-11-24 02:34:41 +0900

    スライドショー実装

--- src/bgchanger.ts
+++ src/bgchanger.ts
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import moment from 'moment';
 import CryptoJS from 'crypto-js';
 
-const random = Math.floor( Math.random() * 5 );
+
+const random = Math.floor( Math.random() * 19 );
 //images
 const normal = require(`./images/${ random }.png`);
 
@@ -17,3 +18,21 @@ if(img !== undefined) {
 
 }
 
+setInterval(() => {
+    const random = Math.floor( Math.random() * 19 );
+    //images
+    const normal = require(`./images/${ random }.png`);
+
+    let img: any | undefined = normal;
+
+    $('.neko').fadeOut(1000, () => {
+        if(img !== undefined) {
+            $('.neko').css({
+                'background-image' : `url(${img})`
+    
+            });
+    
+        }
+    }).fadeIn()
+
+}, 10000)
