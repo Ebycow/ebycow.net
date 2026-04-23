736ff15b4ff71bbc0755ff2f99c46189bd94c794
Ebycow <18446038+Ebycow@users.noreply.github.com>
2026-04-24 00:36:59 +0900

    清掃

--- memo.md
+++
@@ -1,1 +0,0 @@
-* google-fonts-webpack-pluginうごかねえ
--- src/art.ts
+++
@@ -1,5 +0,0 @@
-import $ from 'jquery';
-
-$('.right-img').fadeOut(0).each(function (index, elem) {
-    $(elem).fadeIn(1000 + index * 1000);
-});
